f05f86c511756b2eb910d73075db1651139dea96
simmedia <simadurlan92@gmail.com>
2020-02-18 11:42:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,161 @@
+<template>
+  <v-app>
+    <span class="bg"></span>
+    <v-navigation-drawer temporary v-model="drawer" app>
+      <v-list dense>
+        <v-list-item router style="cursor: pointer">
+          <v-list-item-content>
+            <v-list-item-title>Menu</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app color="green lighten-1" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          contain
+          src="@/assets/images/pizzaLogo.png"
+          transition="scale-transition"
+          width="120"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn text>
+          <span class="mr-2">MENU</span>
+        </v-btn>
+        <v-btn text>
+          <span class="mr-2">ABOUT US</span>
+        </v-btn>
+        <v-btn text>
+          <span class="mr-2">DELIVERY</span>
+        </v-btn>
+        <v-btn text>
+          <span class="mr-2">CONTACT</span>
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-menu transition="scroll-x-reverse-transition" v-model="value" :close-on-content-click="closeOnContentClick">
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" text>
+            <v-icon>mdi-cart</v-icon>
+          </v-btn>
+        </template>
+      <v-card
+      class="mx-auto"
+      max-width="400"
+      tile
+    >
+      <v-list
+        :two-line="twoLine"
+        :avatar="avatar"
+      >
+        <v-subheader>
+          <h1 class="title">Cart</h1>
+        </v-subheader>
+        <v-list-item-group v-model="item" color="primary">
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-avatar v-if="avatar">
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-html="item.title"></v-list-item-title>
+              <v-list-item-subtitle v-if="twoLine || threeLine" v-html="item.subtitle"></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-switch class="pl-4" color="red" v-model="takeOut" label="Take Out" :value="takeOut"></v-switch>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+      </v-menu>
+      <v-app-bar-nav-icon
+        class="hidden-sm-and-up"
+        @click.stop="drawer = !drawer"
+      />
+    </v-app-bar>
+    <v-content>
+      <v-container class="appContainer" fluid>
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    drawer: false,
+    value: false,
+    takeOut: false,
+    closeOnContentClick: false,
+    item: 5,
+      items: [
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'Birthday gift',
+          subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          title: 'Recipe to try',
+          subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+        },
+      ],
+      twoLine: true,
+      avatar: true,
+  })
+};
+</script>
+
+<style lang="scss">
+.appContainer {
+  padding: 0px !important;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.v-app-bar,
+.v-toolbar__content {
+  height: 75px !important;
+}
+
+.bg {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("assets/images/pizza_logo.png") repeat center center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="home">
+    <v-container class="main" mt-12>
+      <v-row wrap class="mt-12">
+        <v-col xs="12" sm="6" class="jumbotron mt-12 order-sm-1">
+          <h1 class="mainText display-2">Delicious and Tasty!</h1>
+          <p class="mt-10 text--secondary subtitle-1">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio rerum
+            accusantium qui, obcaecati numquam doloribus neque molestiae
+            architecto quia, voluptate asperiores eos placeat error est!
+          </p>
+          <v-btn color="my-10 red">
+            <span class="mr-2 white--text">ORDER NOW</span>
+          </v-btn>
+        </v-col>
+        <v-col align="center" md="6">
+          <img
+            src="@/assets/images/pizza_chief.png"
+            width="300px"
+            class="ml-12 chiefImg"
+            alt=""
+            transition="scale-transition"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container mt-12 fluid class="red">
+      <v-row wrap justify="center">
+        <v-col md="3" cols="12" class="yellow"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+          suscipit.</v-col
+        >
+        <v-col md="3" cols="12" class="blue"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+          suscipit.</v-col
+        >
+        <v-col md="3" cols="12" class="green"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+          suscipit.</v-col
+        >
+      </v-row>
+    </v-container>
+    <v-container height="100vh">
+      <v-row justify="center">
+        <img class="three-icon-pack" width="400px" src="../assets/images/threeIconPack.png" alt="" />
+      </v-row>
+    </v-container>
+    <v-container fluid>
+      <v-carousel
+        cycle
+        height="400"
+        hide-delimiter-background
+        show-arrows-on-hover
+      >
+        <v-carousel-item v-for="(slide, i) in slides" :key="i">
+          <v-sheet :color="colors[i]" height="100%">
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="display-3">{{ slide }} Slide</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      colors: [
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4"
+      ],
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  height: 100vh !important;
+  display: flex;
+  align-items: center;
+
+  .mainText {
+    color: #424242;
+  }
+}
+@media screen and (max-width: 600px) {
+  .chiefImg {
+    width: 200px;
+    margin-top: -50px;
+  }
+  .jumbotron {
+    text-align: center;
+    p {
+      text-align: left;
+    }
+  }
+  .three-icon-pack {
+    width: 300px;
+  }
+}
+</style>
